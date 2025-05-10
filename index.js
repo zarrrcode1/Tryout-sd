d9e885d8ebdf06e6562b81884c17ca5f7b4da268
zarrrcode1 <akunperibadike420@gmail.com>
2025-05-10 20:42:27 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+const box = 20;
+let snake = [{ x: 9 * box, y: 10 * box }];
+let direction = 'RIGHT';
+let food = {
+    x: Math.floor(Math.random() * 20) * box,
+    y: Math.floor(Math.random() * 20) * box
+};
+let score = 0;
+
+document.addEventListener('keydown', changeDirection);
+
+function changeDirection(event) {
+    if (event.key === 'ArrowUp' && direction !== 'DOWN') direction = 'UP';
+    else if (event.key === 'ArrowDown' && direction !== 'UP') direction = 'DOWN';
+    else if (event.key === 'ArrowLeft' && direction !== 'RIGHT') direction = 'LEFT';
+    else if (event.key === 'ArrowRight' && direction !== 'LEFT') direction = 'RIGHT';
+}
+
+function drawGame() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i === 0 ? 'green' : 'white';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(food.x, food.y, box, box);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction === 'UP') snakeY -= box;
+    if (direction === 'DOWN') snakeY += box;
+    if (direction === 'LEFT') snakeX -= box;
+    if (direction === 'RIGHT') snakeX += box;
+
+    if (snakeX === food.x && snakeY === food.y) {
+        score++;
+        document.getElementById('score').innerText = score;
+        food = {
+            x: Math.floor(Math.random() * 20) * box,
+            y: Math.floor(Math.random() * 20) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    const newHead = { x: snakeX, y: snakeY };
+
+    if (
+        snakeX < 0 || snakeY < 0 ||
+        snakeX >= canvas.width || snakeY >= canvas.height ||
+        snake.some(segment => segment.x === newHead.x && segment.y === newHead.y)
+    ) {
+        clearInterval(game);
+        alert('Game Over!');
+    }
+
+    snake.unshift(newHead);
+}
+
+const game = setInterval(drawGame, 100);
